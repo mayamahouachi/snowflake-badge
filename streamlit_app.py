59d55ff994a02cf8b7934a0522ce0658ac077c7c
Maya Mahouachi <120915561+mayamahouachi@users.noreply.github.com>
2026-03-17 16:45:14 +0100

    Refactor ingredient selection and display logic
    Updated the multiselect component to use a list of fruit names from the DataFrame and improved the handling of ingredient selection.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,26 +26,35 @@ my_dataframe = session.table("smoothies.public.fruit_options") \
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
+pd_df.columns = [col.upper() for col in pd_df.columns]
 
-#multiselect
-ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe.select(col('FRUIT_NAME'),
-                               max_selections=5)
-#inspect ingredients_list
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    pd_df['FRUIT_NAME'].tolist(),
+    max_selections=5
+)
+
 if ingredients_list:
     ingredients_string=''
-    st.write(pd_df.columns.tolist())
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen+' '
-        pd_df.columns = [col.upper() for col in pd_df.columns]
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.subheader(fruit_chosen+' Nutrition Information')
-        smoothiefroot_response =  requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}") 
-        st_df = st.dataframe(data=smoothiefroot_response.json(),  width='stretch')
+        ingredients_string += fruit_chosen + ' '
 
+        search_on = pd_df.loc[
+            pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'
+        ].iloc[0]
+
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.subheader(fruit_chosen + ' Nutrition Information')
+
+        smoothiefroot_response = requests.get(
+            f"https://my.smoothiefroot.com/api/fruit/{search_on}"
+        )
+
+        st.dataframe(
+            data=smoothiefroot_response.json(),
+            use_container_width=True
+        )
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                         values ('""" + ingredients_string + """'"""+""", '""" +name_on_order+"""')"""
