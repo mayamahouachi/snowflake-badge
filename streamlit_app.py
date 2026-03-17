6aac85cfa4512cf043a895098402c64e949e4c4a
Maya Mahouachi <120915561+mayamahouachi@users.noreply.github.com>
2026-03-17 15:03:41 +0100

    Add API call to fetch orange fruit data
    Fetch and display response from Smoothiefroot API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/orange](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 # Write directly to the app
 st.title(f"Customize your smoothie! :cup_with_straw: ")
 st.write(
