750edcb3b99ad96ca9c5b7dd4193e9745d300b73
Maya Mahouachi <120915561+mayamahouachi@users.noreply.github.com>
2026-03-17 16:20:22 +0100

    Switch dataframe display to use pandas
    Comment out the original dataframe display and convert to pandas.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,11 @@ session = cnx.session()
 my_dataframe= session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 
 #table
-st.dataframe(data=my_dataframe, use_container_width=True) 
+#st.dataframe(data=my_dataframe, use_container_width=True) 
+#st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 
