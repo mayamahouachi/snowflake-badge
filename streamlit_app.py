8db5deeab4f24eaeb4b6725a8dededa547df3ac6
Maya Mahouachi <120915561+mayamahouachi@users.noreply.github.com>
2026-03-17 16:31:00 +0100

    Fix formatting and update fruit search functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+' '
+        pd_df.columns = [col.upper() for col in pd_df.columns]
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen+' Nutrition Information')
